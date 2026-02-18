b976a3ef909ae36d56be205d679e2e3a7216859b
08codershruti <shrutisoni058@gmail.com>
2026-02-18 22:19:31 +0530

    initial commit

---
+++ src/main/java/com/ecoms/ecomProj/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.ecoms.ecomProj.service;
+
+
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ecoms.ecomProj.model.Product;
+import com.ecoms.ecomProj.repo.ProductRepo;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepo repo;
+    
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(int prodId) {
+        return repo.findById(prodId).orElse(new Product());
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int productId, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+
+}
